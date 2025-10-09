b4b5ad4323663c644bc143ebe2263c421defb4a1
t-uejo <info@taikiuejo.com>
2025-10-10 08:01:11 +0900

    チーム開発を前提とするためaccess_keyとsecret_keyは削除、リージョンを変更

--- provider.tf
+++ provider.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  access_key = "test"
-  secret_key = "test"
-  region     = "eu-central-1"
+  region     = "ap-northeast-1"
 
   s3_use_path_style           = true
   skip_requesting_account_id  = true
